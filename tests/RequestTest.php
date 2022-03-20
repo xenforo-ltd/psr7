b36d463b214c76ed12b924bae0445f3531bb1686
Graham Campbell <GrahamCampbell@users.noreply.github.com>
2022-03-20 21:51:23 +0000

    Release 2.1.2 (#492)

--- tests/RequestTest.php
+++ tests/RequestTest.php
@@ -176,6 +176,17 @@ class RequestTest extends TestCase
         ], $r->getHeaders());
     }
 
+    public function testHeaderValueWithWhitespace(): void
+    {
+        $r = new Request('GET', 'https://example.com/', [
+            'User-Agent' => 'Linux f0f489981e90 5.10.104-linuxkit 1 SMP Wed Mar 9 19:05:23 UTC 2022 x86_64'
+        ]);
+        self::assertSame([
+            'Host' => ['example.com'],
+            'User-Agent' => ['Linux f0f489981e90 5.10.104-linuxkit 1 SMP Wed Mar 9 19:05:23 UTC 2022 x86_64']
+        ], $r->getHeaders());
+    }
+
     public function testCanGetHeaderAsCsv(): void
     {
         $r = new Request('GET', 'http://foo.com/baz?bar=bam', [
